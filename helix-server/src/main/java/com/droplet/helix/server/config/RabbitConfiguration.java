60728d6b1209a001610aa4eaa164c9ed7fba1680
霜暮雪 <Frost.rA9@gmail.com>
2023-12-14 05:05:10 +0800

    feat : web

--- helix-server/src/main/java/com/droplet/helix/server/config/RabbitConfiguration.java
+++ helix-server/src/main/java/com/droplet/helix/server/config/RabbitConfiguration.java
@@ -2,6 +2,8 @@ package com.droplet.helix.server.config;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,5 +17,10 @@ public class RabbitConfiguration {
         return QueueBuilder
                 .durable("mail")
                 .build();
+    }
+
+    @Bean
+    public MessageConverter messageConverter(){
+        return new Jackson2JsonMessageConverter();
     }
 }
