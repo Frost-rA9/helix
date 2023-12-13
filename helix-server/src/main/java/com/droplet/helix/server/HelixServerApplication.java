6a116719487203cea97b5a1750fab6a308db49b1
霜暮雪 <Frost.rA9@gmail.com>
2023-12-14 00:58:40 +0800

    init : init commit

---
+++ helix-server/src/main/java/com/droplet/helix/server/HelixServerApplication.java
@@ -0,0 +1,13 @@
+package com.droplet.helix.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelixServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelixServerApplication.class, args);
+    }
+
+}
